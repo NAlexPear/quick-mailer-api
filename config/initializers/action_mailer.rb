51712e26d9df6d609050550d4f1189a05ccd763c
Alex Pearson <alex@alexpear.com>
2018-10-01 12:27:43 -0500

    :hammer: Handle Sidekiq errors with more idiomatic rescue_from method

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,3 +1,13 @@
-# ActionMailer::DeliveryJob.rescue_from(StandardError) do |error|
-#   puts "error from mailer: #{error}"
-# end
+ActionMailer::DeliveryJob.rescue_from(StandardError) do |error|
+  # @TODO: add more granular error handling
+  # e.g. SMTP vs Network vs configuration errors
+
+  message = arguments.last
+
+  if message.kind_of?(Message)
+    error = Error.create!(message_id: message.id,
+                          failure_type: :network)
+
+    message.update(error_id: error.id)
+  end
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,5 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['JOB_WORKER_URL'] }
-  config.error_handlers << Proc.new do |error, context|
-    gid = context[:job]['args'][0]['arguments'].last['_aj_globalid'].split('/').last
-
-    message = Message.find(gid)
-
-    unless message.nil?
-      error = Error.create!(message_id: message.id,
-                            failure_type: :network)
-
-      message.update(error_id: error.id)
-    end
-  end
 end
 
 Sidekiq.configure_client do |config|
