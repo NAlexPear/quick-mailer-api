2ce78761de688796f7c6060ee429830d14ac3614
Alex Pearson <alex@alexpear.com>
2018-10-01 12:35:50 -0500

    :bomb: Prune unnecessary queue tests

--- spec/mailers/generic_mailer_spec.rb
+++ spec/mailers/generic_mailer_spec.rb
@@ -17,20 +17,5 @@ RSpec.describe GenericMailer, :type => :mailer do
       expect(email.to).to eq([@params[:email]])
       expect(email.subject).to eq(@params[:subject])
     end
-
-    it 'can deliver an email later' do
-      delivery = instance_double(ActionMailer::MessageDelivery)
-
-      expect(GenericMailer).to receive(:email).with(@params).and_return(delivery)
-      expect(delivery).to receive(:deliver_later)
-
-      GenericMailer.with(@params).email.deliver_later
-    end
-
-    it 'enqueues an email through Sidekiq' do
-      expect {
-        GenericMailer.with(@params).email.deliver_later
-      }.to change(Sidekiq::Worker.jobs, :size).by(1)
-    end
   end
 end
